1fba3fdee8d0553f4eabe5b2d3dd1b5b4a5cc35a
Greg Rubin <rubin@amazon.com>
2019-07-30 14:26:38 -0700

    Fix security properties file for #46.
    JDK8 and JDK9+ specify providers in the properties files differently.
    This modifies our .security file to work properly in both version.

--- tst/com/amazon/corretto/crypto/provider/test/SecurityPropertyTester.java
+++ tst/com/amazon/corretto/crypto/provider/test/SecurityPropertyTester.java
@@ -5,8 +5,11 @@ package com.amazon.corretto.crypto.provider.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.security.KeyPairGenerator;
 import java.security.Provider;
 import java.security.Security;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.amazon.corretto.crypto.provider.AmazonCorrettoCryptoProvider;
 
@@ -14,10 +17,22 @@ import com.amazon.corretto.crypto.provider.AmazonCorrettoCryptoProvider;
  * This is a special stand-alone test case which asserts that AmazonCorrettoCryptoProvider is installed as the highers priority provider and is functional.
  */
 public class SecurityPropertyTester {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     final Provider provider = Security.getProviders()[0];
     assertEquals("AmazonCorrettoCryptoProvider", provider.getName());
     final AmazonCorrettoCryptoProvider njb = (AmazonCorrettoCryptoProvider) provider;
     njb.assertHealthy();
+
+    // We know that Java has the SunEC provider which can generate EC keys.
+    // We try to grab it to show that the nothing interfered with proper provider loading.
+    KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC", "SunEC");
+
+    // Also ensure that nothing shows up twice
+    Set<String> names = new HashSet<>();
+    for (Provider p : Security.getProviders()) {
+        if (!names.add(p.getName())) {
+            throw new AssertionError("Duplicate found for " + p.getName());
+        }
+    }
   }
 }
