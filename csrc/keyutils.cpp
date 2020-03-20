6c9f5eb85da89c0c955f741189f0e94c88d68686
Andrew Hopkins <andhop@amazon.com>
2020-03-19 18:29:31 -0700

    Add test for keyutils der2EvpPrivateKey and fix additional leaked OpenSSL error

--- csrc/keyutils.cpp
+++ csrc/keyutils.cpp
@@ -24,15 +24,15 @@ EVP_PKEY* der2EvpPrivateKey(const unsigned char* der, const int derLen, const bo
     if (pkcs8Key) {
       PKCS8_PRIV_KEY_INFO_free(pkcs8Key);
     }
-    throw_java_ex(javaExceptionClass, "Extra key information");
+      throw_openssl(javaExceptionClass, "Extra key information");
   }
   if (!pkcs8Key) {
-    throw_openssl(javaExceptionClass, "Unable to parse DER key into PKCS8_PRIV_KEY_INFO");
+      throw_openssl(javaExceptionClass, "Unable to parse DER key into PKCS8_PRIV_KEY_INFO");
   }
   EVP_PKEY* result = EVP_PKCS82PKEY(pkcs8Key);
   PKCS8_PRIV_KEY_INFO_free(pkcs8Key);
   if (!result) {
-    throw_openssl(javaExceptionClass, "Unable to convert PKCS8_PRIV_KEY_INFO to EVP_PKEY");
+      throw_openssl(javaExceptionClass, "Unable to convert PKCS8_PRIV_KEY_INFO to EVP_PKEY");
   }
 
   if (checkPrivateKey && !checkKey(result)) {
