aac0268ecec2e79ba3c19cc925174ac038d4876e
Andrew Hopkins <andhop@amazon.com>
2020-03-20 13:56:35 -0700

    Rename ENABLE_RNG_TEST_HOOKS to ENABLE_NATIVE_TEST_HOOKS

--- csrc/rdrand.h
+++ csrc/rdrand.h
@@ -20,7 +20,7 @@ bool supportsRdSeed();
 
 }
 
-#ifdef ENABLE_RNG_TEST_HOOKS
+#ifdef ENABLE_NATIVE_TEST_HOOKS
 extern "C" {
     extern bool (*hook_rdrand)(uint64_t *out);
     extern bool (*hook_rdseed)(uint64_t *out);
