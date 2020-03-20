10c56a57f24522cef6aed19194b6217a93cf8279
Andrew Hopkins <andhop@amazon.com>
2020-03-20 14:38:31 -0700

    Check OpenSSL error after every native test instead of once per test suite

--- csrc/test_utils.h
+++ csrc/test_utils.h
@@ -7,40 +7,36 @@
 #include <openssl/err.h>
 
 bool test_succeeded = true;
-bool success = true;
+bool test_suite_success = true;
 
 #define BEGIN_TEST() \
   do { \
-    success = true; \
+    test_suite_success = true; \
   } while(0)
 
 #define END_TEST() \
 do { \
-    bool errorFound = false; \
-    const char* file; \
-    int line; \
-    unsigned long unhandledError = ERR_get_error_line(&file, &line); \
-    while (unhandledError) { \
-        errorFound = true; \
-        std::cerr << "Found unhandled openssl error: " << formatOpensslError(unhandledError, "NO_TEXT"); \
-        std::cerr << " @ " << file << ":" << line << std::endl; \
-        unhandledError = ERR_get_error_line(&file, &line); \
-    } \
-    if (errorFound) { \
-        FAIL(); \
-    } \
-    return success ? 0 : 1; \
+    return test_suite_success ? 0 : 1; \
   } while(0)
 
 #define RUNTEST(name) do { \
     printf("Running test %s... ", #name); \
     test_succeeded = true; \
     name(); \
+    const char* file; \
+    int line; \
+    unsigned long unhandledError = ERR_get_error_line(&file, &line); \
+    while (unhandledError) { \
+        test_succeeded = false; \
+        std::cerr << "Found unhandled openssl error: " << formatOpensslError(unhandledError, "NO_TEXT"); \
+        std::cerr << " @ " << file << ":" << line << std::endl; \
+        unhandledError = ERR_get_error_line(&file, &line); \
+    } \
     if (test_succeeded) { \
         printf("ok\n"); \
     } else {\
         printf("FAILED test %s\n", #name); \
-        success = false; \
+        test_suite_success = false; \
     } \
 } while (0)
 
@@ -53,7 +49,7 @@ do { \
 
 #define FAIL() do { \
     test_succeeded = false; \
-    success = false; \
+    test_suite_success = false; \
     printf("Failed assertion at %s:%d\n", __FILE__, __LINE__); \
 } while (0)
 
