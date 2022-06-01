5632b00a4b3fc1207386f1fd661fe67d22ef9d1e
samuel40791765 <sachiang@amazon.com>
2022-06-01 22:52:22 +0000

    split CI into different jdk versions

--- tests/ci/run_accp_test_integration.sh
+++ tests/ci/run_accp_test_integration.sh
@@ -3,11 +3,15 @@ set -exo pipefail
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-echo "Testing ACCP integration tests with corretto 11."
-./gradlew test_integration
+if type -p java; then
+	# Parse and check which Java version we're using.
+    version=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
 
-echo "Testing ACCP integration tests with corretto 8."
-./gradlew -DTEST_JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto test_integration
-
-echo "Testing ACCP integration tests with corretto 17."
-./gradlew -DTEST_JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto -DTEST_JAVA_MAJOR_VERSION=17 test_integration
+    # This flag is necessary in Java17+ for certain unit tests to
+	# perform deep reflection on nonpublic members.
+    if (( "$version" >= "17" )); then
+        ./gradlew -DTEST_JAVA_MAJOR_VERSION=$version test_integration
+    else
+        ./gradlew test_integration
+    fi
+fi
