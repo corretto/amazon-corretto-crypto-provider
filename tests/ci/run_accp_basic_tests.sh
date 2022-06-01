e0b273e99b314cc84a59122b455b55225ff4fcf5
samuel40791765 <sachiang@amazon.com>
2022-06-01 23:17:04 +0000

    let the script point to the correct java path

--- tests/ci/run_accp_basic_tests.sh
+++ tests/ci/run_accp_basic_tests.sh
@@ -3,8 +3,12 @@ set -exo pipefail
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+# Assign the JAVA_HOME variable to the path. Otherwise, Ubuntu will
+# default to the newest version of Java.
+export PATH=$JAVA_HOME/bin:$PATH
+
 if type -p java; then
-	# Parse and check which Java version we're using.
+    # Parse and check which Java version we're using.
     version=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
 
     # This flag is necessary in Java17+ for certain unit tests to
