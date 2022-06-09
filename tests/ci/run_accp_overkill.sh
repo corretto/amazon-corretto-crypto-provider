7e18a93293d03e3f39a9d89430feed991a49934e
Samuel Chiang <samuelchungchiang@gmail.com>
2022-06-09 16:21:29 -0700

    Add FIPS support to ACCP CI (#218)
    We now have support for FIPS in ACCP. This commit adds FIPS 
    support to our CI to ensure we have continuous integration tests
    running on the FIPS dimension. CI Readme has also been updated
    to reflect the newest state.

--- tests/ci/run_accp_overkill.sh
+++ tests/ci/run_accp_overkill.sh
@@ -3,11 +3,27 @@ set -exo pipefail
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+# Testing non-FIPS is the default.
+testing_fips=false
+while [[ $# -gt 0 ]]; do
+    case ${1} in
+    --fips)
+      testing_fips=true
+      ;;
+    *)
+      echo "${1} is not supported."
+      exit 1
+      ;;
+    esac
+    # Check next option -- key/value.
+    shift
+done
+
 echo "Testing ACCP overkill tests."
 
 # dieharder_threads are not supported on ARM for now.
 if [[ ("$(uname -p)" == 'aarch64'*) || ("$(uname -p)" == 'arm'*) ]]; then
-	./gradlew test_extra_checks test_integration_extra_checks
+	./gradlew -DFIPS=$testing_fips test_extra_checks test_integration_extra_checks
 else
-	./gradlew test_extra_checks test_integration_extra_checks dieharder_threads
+	./gradlew -DFIPS=$testing_fips test_extra_checks test_integration_extra_checks dieharder_threads
 fi
