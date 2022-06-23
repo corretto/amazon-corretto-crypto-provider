b6ed1731b773c590cc0a41f113968c4c4b395268
samuel40791765 <sachiang@amazon.com>
2022-06-23 00:22:06 +0000

    save progress

--- tests/ci/cdk/app.py
+++ tests/ci/cdk/app.py
@@ -7,8 +7,9 @@ from aws_cdk import core
 
 from cdk.accp_github_ci_stack import ACCPGitHubCIStack
 from cdk.linux_docker_image_batch_build_stack import LinuxDockerImageBatchBuildStack
+from cdk.windows_docker_image_build_stack import WindowsDockerImageBuildStack
 from cdk.ecr_stack import EcrStack
-from util.metadata import AWS_ACCOUNT, AWS_REGION, LINUX_ECR_REPO
+from util.metadata import AWS_ACCOUNT, AWS_REGION, LINUX_ECR_REPO, WINDOWS_X86_ECR_REPO
 
 # Initialize app.
 app = core.App()
@@ -19,9 +20,14 @@ env = core.Environment(account=AWS_ACCOUNT, region=AWS_REGION)
 # Define AWS ECR stacks.
 # ECR holds the docker images, which are pre-built to accelerate the code builds/tests of git pull requests.
 EcrStack(app, "accp-ecr-linux-all", LINUX_ECR_REPO, env=env)
+EcrStack(app, "accp-ecr-windows-x86", WINDOWS_X86_ECR_REPO, env=env)
 
 # Define CodeBuild Batch job for building Docker images.
 LinuxDockerImageBatchBuildStack(app, "accp-docker-image-build-linux", env=env)
+
+# AWS CodeBuild cannot build Windows Docker images because DIND (Docker In Docker) is not supported on Windows.
+# Windows Docker images are created by running commands in Windows EC2 instance.
+WindowsDockerImageBuildStack(app, "accp-docker-image-build-windows", env=env)
 
 # Define CodeBuild Batch job for testing code.
 x86_build_spec_file = "./cdk/codebuild/pr_integration_linux_x86_omnibus.yaml"
@@ -30,5 +36,7 @@ arm_build_spec_file = "./cdk/codebuild/pr_integration_linux_arm_omnibus.yaml"
 ACCPGitHubCIStack(app, "accp-ci-pr-integration-linux-arm", LINUX_ECR_REPO, arm_build_spec_file, env=env)
 extra_build_spec_file = "./cdk/codebuild/dieharder_overkill_omnibus.yaml"
 ACCPGitHubCIStack(app, "accp-ci-overkill-dieharder", LINUX_ECR_REPO, extra_build_spec_file, env=env)
+win_x86_build_spec_file = "./cdk/codebuild/pr_integration_windows_x86_omnibus.yaml"
+ACCPGitHubCIStack(app, "accp-ci-pr-integration-windows-x86", WINDOWS_X86_ECR_REPO, win_x86_build_spec_file, env=env)
 
 app.synth()
---
+++ tests/ci/cdk/cdk/windows_docker_image_build_stack.py
@@ -0,0 +1,62 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from aws_cdk import core, aws_ec2 as ec2, aws_s3 as s3, aws_iam as iam, aws_ssm as ssm
+from util.iam_policies import ecr_power_user_policy_in_json, s3_read_write_policy_in_json
+from util.metadata import AWS_ACCOUNT, AWS_REGION, WINDOWS_X86_ECR_REPO, S3_BUCKET_NAME, GITHUB_REPO_OWNER, WIN_EC2_TAG_KEY, \
+    WIN_EC2_TAG_VALUE, SSM_DOCUMENT_NAME, GITHUB_SOURCE_VERSION
+from util.yml_loader import YmlLoader
+
+
+class WindowsDockerImageBuildStack(core.Stack):
+    """Define a temporary stack used to build Windows Docker images. After build, this stack will be destroyed."""
+
+    def __init__(self,
+                 scope: core.Construct,
+                 id: str,
+                 **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Define SSM command document.
+        ecr_repo = "{}.dkr.ecr.{}.amazonaws.com/{}".format(AWS_ACCOUNT, AWS_REGION, WINDOWS_X86_ECR_REPO)
+        placeholder_map = {"ECR_PLACEHOLDER": ecr_repo, "GITHUB_OWNER_PLACEHOLDER": GITHUB_REPO_OWNER,
+                           "REGION_PLACEHOLDER": AWS_REGION, "GITHUB_SOURCE_VERSION_PLACEHOLDER": GITHUB_SOURCE_VERSION}
+        content = YmlLoader.load("./cdk/ssm/windows_docker_build_ssm_document.yaml", placeholder_map)
+        ssm.CfnDocument(scope=self,
+                        id="{}-ssm-document".format(id),
+                        name=SSM_DOCUMENT_NAME,
+                        content=content,
+                        document_type="Command")
+
+        # Define a S3 bucket to store windows docker files and build scripts.
+        s3.Bucket(scope=self,
+                  id="{}-s3".format(id),
+                  bucket_name=S3_BUCKET_NAME,
+                  block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+
+        # Define a role for EC2.
+        ecr_power_user_policy = iam.PolicyDocument.from_json(ecr_power_user_policy_in_json([WINDOWS_X86_ECR_REPO]))
+        s3_read_write_policy = iam.PolicyDocument.from_json(s3_read_write_policy_in_json(S3_BUCKET_NAME))
+        inline_policies = {"ecr_power_user_policy": ecr_power_user_policy, "s3_read_write_policy": s3_read_write_policy}
+        role = iam.Role(scope=self, id="{}-role".format(id),
+                        assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+                        inline_policies=inline_policies,
+                        managed_policies=[
+                            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore")
+                        ])
+
+        # Define Windows EC2 instance, where the SSM document will be executed.
+        machine_image = ec2.MachineImage.latest_windows(ec2.WindowsVersion.WINDOWS_SERVER_2019_ENGLISH_FULL_CONTAINERSLATEST)
+        vpc = ec2.Vpc(scope=self, id="{}-vpc".format(id))
+        block_device_volume = ec2.BlockDeviceVolume.ebs(volume_size=200, delete_on_termination=True)
+        block_device = ec2.BlockDevice(device_name="/dev/sda1", volume=block_device_volume)
+        instance = ec2.Instance(scope=self,
+                                id="{}-instance".format(id),
+                                instance_type=ec2.InstanceType(instance_type_identifier="m5d.xlarge"),
+                                vpc=vpc,
+                                role=role,
+                                block_devices=[block_device],
+                                vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+                                machine_image=machine_image)
+
+        core.Tags.of(instance).add(WIN_EC2_TAG_KEY, WIN_EC2_TAG_VALUE)
