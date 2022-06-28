ee01d96f099cfa440b4c0819734f5458a1a540c3
samuel40791765 <sachiang@amazon.com>
2022-06-28 22:04:06 +0000

    remove extras

--- tests/ci/docker_images/windows/build_images.ps1
+++ tests/ci/docker_images/windows/build_images.ps1
@@ -1,6 +1,6 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-docker build -t accp/windows_base:2019 -f .\windows_base\DockerFile ../../../../
+docker build -t accp/windows_base:2019 .\windows_base
 docker build -t vs2015_corretto .\vs2015_corretto
 docker build -t vs2017_corretto .\vs2017_corretto
