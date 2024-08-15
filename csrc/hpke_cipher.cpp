8cd1de66af66eddafdc878559ceddfb99b982f13
sanketh <sgmenda@amazon.com>
2024-08-15 02:02:08 -0400

    implement cpp for hpke keygen

---
+++ csrc/hpke_cipher.cpp
@@ -0,0 +1,13 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+#include "auto_free.h"
+#include "bn.h"
+#include "buffer.h"
+#include "env.h"
+#include "generated-headers.h"
+#include "keyutils.h"
+#include "util.h"
+#include <openssl/evp.h>
+#include <openssl/nid.h>
+
+using namespace AmazonCorrettoCryptoProvider;
