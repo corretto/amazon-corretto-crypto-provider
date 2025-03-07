3739dc89d6717e98b1d6f8ea09646ba3b3aeeec9
Will Childs-Klein <childw@amazon.com>
2025-03-07 17:20:27 -0500

    Add build option for exposing self-test failure messages (#433)
    *Issue #, if available:* ACCP-129
    
    _**THIS FEATURE IS NOT INTENDED FOR PRODUCTION AND IS THUS LEFT
    UNDOCUMENTED IN OUR README**_
    
    ## Notes
    
    This PR builds on prior work from @amirhosv on the
    `fips-experimentation` branch to provide an alternative mode of handling
    AWS-LC self test failures. We call this (non-default) mode
    `FIPS_SELF_TEST_SKIP_ABORT`, and it is only usable when ACCP is built in
    FIPS mode.
    
    By default, AWS-LC will `abort()` on self-test failures. However, as of
    AWS-LC v1.47, when built with the `AWSLC_FIPS_FAILURE_CALLBACK` build
    flag AWS-LC will [call][1] a [weak symbol][2]
    `AWS_LC_fips_failure_callback` function to handle self test failures
    instead of aborting. When ACCP is built with
    `-DFIPS_SELF_TEST_SKIP_ABORT`, ACCP defines
    `AWS_LC_fips_failure_callback` such that it appends to a queue of error
    strings in ACCP's native heap. To manage the accumulated error strings,
    we add a native `std::vector` wrapper `ConcurrentStringVector` providing
    a minimal, threadsafe API. Once the error queue is non-empty, all
    subsequent `getInstance` calls on an algorithm provided by ACCP will
    throw `FipsStatusException`.
    
    We provide two functions for callers to query fips self test error state
    on an `AmazonCorrettoCryptoProvider` instance:
    
    - `public boolean isFipsStatusOk()`
    - `public List<String> getFipsSelfTestFailures()`
    
    We could get away with only the latter function, but we provide
    `isFipsStatusOk()` to avoid performance costs of copying error strings
    over the JNI.
    
    
    [1]:
    https://github.com/aws/aws-lc/blob/1d8b807ed1ae75c89beda6c73a4ae27c404fa46f/crypto/fipsmodule/bcm.c#L416
    [2]:
    https://github.com/aws/aws-lc/blob/1d8b807ed1ae75c89beda6c73a4ae27c404fa46f/crypto/internal.h#L1427-L1432
    
    ## Testing
    
    To adequately test the new mode, we need to build AWS-LC with
    `FIPS_BREAK_TEST=TESTS` to programmatically break AWS-LC's pairwise
    consistency tests (PCTs). We test against each available PCT breakage
    during key generation by setting the appropriate environment variable,
    indicating which PCT to break. Unfortunately, Java doesn't appear to
    have a standard utility for manipulating process environment variables
    at runtime, so we create our own `TestUtil.setEnv` that calls POSIX
    `setenv`/`unsetenv` over the JNI.
    
    In addition to CI tests, I've also executed `run_accp_basic_tests.sh`
    with the new `--fips-self-test-failure-no-abort` flag, which will
    eventually be incorporated into our GitHub CI.
    
    ```
    $ TEST_JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64 ./tests/ci/run_accp_basic_tests.sh --fips-self-test-failure-no-abort
    ...
    BUILD SUCCESSFUL in 40m 15s
    18 actionable tasks: 13 executed, 5 up-to-date
    ```
    
    ---
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Co-authored-by: Amir Vakili <amirhosv@amazon.com>
    Co-authored-by: Amir Vakili <114409643+amirhosv@users.noreply.github.com>
    Co-authored-by: Gerardo Ravago 🇵🇭 <gcr@amazon.com>

---
+++ csrc/fips_status.cpp
@@ -0,0 +1,87 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+#include <cstdio>
+#include <functional>
+#include <jni.h>
+#include <string.h>
+#include <vector>
+
+#include "string_vector.h"
+
+static AmazonCorrettoCryptoProvider::ConcurrentStringVector fipsStatusErrors(1024);
+
+// To have this symbol exported, one needs to modify the final-link.version and the CMakeLists.txt
+extern "C" void AWS_LC_fips_failure_callback(char const* message);
+
+#if defined(FIPS_SELF_TEST_SKIP_ABORT)
+void AWS_LC_fips_failure_callback(char const* message)
+{
+    const size_t char_limit = 128;
+    if (strnlen(message, char_limit + 1) > char_limit) {
+        fprintf(stderr, "AWS_LC_fips_failure_callback invoked with message message exceeding %lu chars\n", char_limit);
+        return;
+    }
+    fprintf(stderr, "AWS_LC_fips_failure_callback invoked with message: '%s'\n", message);
+    fipsStatusErrors.push_back(message);
+}
+#else
+void AWS_LC_fips_failure_callback(char const* message)
+{
+    fprintf(stderr, "AWS_LC_fips_failure_callback invoked with message: '%s'\n", message);
+    abort();
+}
+#endif
+
+extern "C" JNIEXPORT jobject JNICALL
+Java_com_amazon_corretto_crypto_provider_AmazonCorrettoCryptoProvider_getFipsSelfTestFailuresInternal(
+    JNIEnv* env, jobject thisObj)
+{
+    std::vector<std::string> errors = fipsStatusErrors.to_std();
+
+    // Construct a Java ArrayList, get a handle for the |add| method
+    jclass arrayListClass = env->FindClass("java/util/ArrayList");
+    if (arrayListClass == NULL) {
+        abort();
+    }
+    jmethodID constructor = env->GetMethodID(arrayListClass, "<init>", "()V");
+    if (constructor == NULL) {
+        abort();
+    }
+    jobject arrayList = env->NewObject(arrayListClass, constructor);
+    if (arrayList == NULL) {
+        abort();
+    }
+    jmethodID addMethod = env->GetMethodID(arrayListClass, "add", "(Ljava/lang/Object;)Z");
+    if (addMethod == NULL) {
+        abort();
+    }
+
+    // Copy the errors over to |arrayList| and clean up temporary local reference
+    for (size_t i = 0; i < errors.size(); i++) {
+        jstring javaString = env->NewStringUTF(errors[i].c_str());
+        env->CallVoidMethod(arrayList, addMethod, javaString);
+        env->DeleteLocalRef(javaString);
+    }
+
+    return arrayList;
+}
+
+extern "C" JNIEXPORT int JNICALL
+Java_com_amazon_corretto_crypto_provider_AmazonCorrettoCryptoProvider_fipsStatusErrorCount(JNIEnv* env, jobject thisObj)
+{
+    return fipsStatusErrors.size();
+}
+
+// TEST methods below
+
+extern "C" JNIEXPORT void JNICALL Java_com_amazon_corretto_crypto_provider_test_NativeTestHooks_resetFipsStatus(
+    JNIEnv*, jclass)
+{
+    fipsStatusErrors.clear();
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_amazon_corretto_crypto_provider_test_NativeTestHooks_callAwsLcFipsFailureCallback(JNIEnv*, jclass)
+{
+    AWS_LC_fips_failure_callback("called by a test");
+}
