ab5ca68bd4f9634abdfb5b36606901feb181a0e4
Will Childs-Klein <childw@amazon.com>
2025-05-01 14:19:53 -0400

    Add CBC benchmarks

---
+++ benchmarks/lib/src/jmh/java/com/amazon/corretto/crypto/provider/benchmarks/AesCbcUpdate.java
@@ -0,0 +1,58 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+package com.amazon.corretto.crypto.provider.benchmarks;
+
+import java.security.spec.AlgorithmParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
+
+import com.amazon.corretto.crypto.provider.AmazonCorrettoCryptoProvider;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+public class AesCbcUpdate extends AesBase {
+  @Param({"128", "256"})
+  public int keyBits;
+
+  @Param({AmazonCorrettoCryptoProvider.PROVIDER_NAME, "BC", "SunJCE"})
+  public String provider;
+
+  @Param({"NoPadding"}) // Other values: { "PKCS5Padding", "ISO10126Padding" }
+  public String padding;
+
+  @Param({"16", "256"})
+  public int chunkSize;
+
+  @Setup
+  public void setup() throws Exception {
+    super.setup(keyBits, provider, padding);
+  }
+
+  @Override
+  protected String getMode() {
+    return "CBC";
+  }
+
+  @Override
+  protected AlgorithmParameterSpec createParameterSpec(byte[] iv) {
+    return new IvParameterSpec(iv);
+  }
+
+  @Override
+  protected int getIvSize() {
+    return 16;
+  }
+
+  @Benchmark
+  public byte[] encrypt() throws Exception {
+    return super.updateEncrypt(chunkSize);
+  }
+
+  @Benchmark
+  public byte[] decrypt() throws Exception {
+    return super.updateDecrypt(chunkSize);
+  }
+}
