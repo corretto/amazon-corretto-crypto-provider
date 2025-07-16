15379ec48bf931a27617edb7b69378f02f4a6516
havishr <hrallabandi@gmail.com>
2025-07-16 08:20:13 -0700

    adjusted the build script for the hash spis to include the sha3 hashing. created the cpp for each hash with the appropriate definitions

---
+++ csrc/sha3_256.cpp
@@ -0,0 +1,3 @@
+#include <openssl/digest.h>
+#define DIGEST_NAME sha3_256 
+#include "hash_evp_template.cpp.template"
---
+++ csrc/sha3_512.cpp
@@ -0,0 +1,3 @@
+#include <openssl/digest.h>
+#define DIGEST_NAME sha3_512
+#include "hash_evp_template.cpp.template"
