103ca48eb05f364f82488e0b6da1c143c3256610
havishr <hrallabandi@gmail.com>
2025-07-22 08:18:54 -0700

    Adjusted variable definitions and shifted CTX definition back onto stack.

--- csrc/sha3_256.cpp
+++ csrc/sha3_256.cpp
@@ -2,4 +2,6 @@
 #define DIGEST_NAME sha3_256 
 #define DIGEST_LENGTH 32
 #define DIGEST_BLOCK_SIZE 136
+#define MD_CTX_SIZE 400
+#define MD_DIGEST_SIZE 32
 #include "hash_evp_template.cpp.template"
--- csrc/sha3_384.cpp
+++ csrc/sha3_384.cpp
@@ -2,4 +2,6 @@
 #define DIGEST_NAME sha3_384 
 #define DIGEST_LENGTH 48
 #define DIGEST_BLOCK_SIZE 104
+#define MD_CTX_SIZE 400
+#define MD_DIGEST_SIZE 48
 #include "hash_evp_template.cpp.template"
