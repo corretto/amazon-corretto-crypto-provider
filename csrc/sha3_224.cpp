0ae51df428656e5e2df43357b543d368455efee3
havishr <hrallabandi@gmail.com>
2025-07-23 13:00:48 -0700

    Replaced duplicate constant

--- csrc/sha3_224.cpp
+++ csrc/sha3_224.cpp
@@ -3,5 +3,4 @@
 #define DIGEST_LENGTH 28
 #define DIGEST_BLOCK_SIZE 144
 #define MD_CTX_SIZE 400
-#define MD_DIGEST_SIZE 28
 #include "hash_evp_template.cpp.template"
--- csrc/sha3_512.cpp
+++ csrc/sha3_512.cpp
@@ -3,5 +3,4 @@
 #define DIGEST_LENGTH 64
 #define DIGEST_BLOCK_SIZE 72
 #define MD_CTX_SIZE 400
-#define MD_DIGEST_SIZE 64
 #include "hash_evp_template.cpp.template"
