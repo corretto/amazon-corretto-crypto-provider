75790ab17af0fd5228c6f934c5641d1abda8f358
prasden <prasden@amazon.com>
2025-08-07 15:21:44 -0700

    spotlessApply

--- src/com/amazon/corretto/crypto/provider/jdk17plus/MlKemSpi.java
+++ src/com/amazon/corretto/crypto/provider/jdk17plus/MlKemSpi.java
@@ -8,7 +8,6 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.spec.AlgorithmParameterSpec;
-import java.security.spec.NamedParameterSpec;
 import javax.crypto.DecapsulateException;
 import javax.crypto.KEM;
 import javax.crypto.KEMSpi;
@@ -27,7 +26,6 @@ abstract class MlKemSpi implements KEMSpi {
 
   private static native void nativeDecapsulate(
       long evpKeyPtr, byte[] ciphertext, byte[] sharedSecret);
-
 
   protected MlKemSpi(MlKemParameter parameterSet) {
     Loader.checkNativeLibraryAvailability();
