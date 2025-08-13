397c6a215bb272fef2e04df85ee309b1c99b41a1
Chocka Chidambaram <7660279+chockalingamc@users.noreply.github.com>
2025-08-13 11:12:01 -0400

    JDK8 compatibility

--- tst/com/amazon/corretto/crypto/provider/test/EvpKeyFactoryTest.java
+++ tst/com/amazon/corretto/crypto/provider/test/EvpKeyFactoryTest.java
@@ -727,11 +727,10 @@ public class EvpKeyFactoryTest {
     // JCE doesn't support ML-DSA until JDK24, and BouncyCastle currently serializes ML-DSA private keys via seeds.
     // TODO: switch to BouncyCastle once BC supports CHOICE-encoded private keys
     // Similarly, JDK doesn't support EdDSA/Ed25519 until JDK15, and XDH/X25519 until JDK11
-    Map<Integer, List<String>> jdkAlgorithmSupport = Map.of(
-            24, Arrays.asList("ML-DSA"),
-            15, Arrays.asList("Ed25519", "Ed25519ph", "EdDSA"),
-            11, Arrays.asList("XDH", "X25519")
-    );
+    Map<Integer, List<String>> jdkAlgorithmSupport = new HashMap<>();
+    jdkAlgorithmSupport.put(24, Arrays.asList("ML-DSA"));
+    jdkAlgorithmSupport.put(15, Arrays.asList("Ed25519", "Ed25519ph", "EdDSA"));
+    jdkAlgorithmSupport.put(11, Arrays.asList("XDH", "X25519"));
     for (Map.Entry<Integer, List<String>> entry : jdkAlgorithmSupport.entrySet()) {
         if (TestUtil.JAVA_VERSION < entry.getKey() && entry.getValue().stream().anyMatch(algorithm::startsWith)) {
             return NATIVE_PROVIDER;
