d15d581e2a87c5a642837f7d3cae0e805d3589cf
prasden <prasden@amazon.com>
2025-09-12 12:42:19 -0700

    Add ML-KEM benchmarks (#495)
    Adds benchmarks for ML-KEM key generation and
    encapsulation/decapsulation.
    
    `MLKEMEncapDecap` must be ran on a build time JDK of 21 or higher as
    BouncyCastle does not register the KEM API on older JDK versions.
    
    <img width="1515" height="465" alt="Screenshot 2025-09-11 at 11 06
    42 PM"
    src="https://github.com/user-attachments/assets/4116b874-2349-48d8-8e46-5940799ff43c"
    />
    
    
    <img width="1508" height="369" alt="Screenshot 2025-09-11 at 11 06
    52 PM"
    src="https://github.com/user-attachments/assets/c43c2a63-e0d6-4573-bf05-55929827148c"
    />
    
    Ran on ARM M3 MacOS.
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

---
+++ benchmarks/lib/src/jmh/java/com/amazon/corretto/crypto/provider/benchmarks/KeyGenMLKEM.java
@@ -0,0 +1,36 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+package com.amazon.corretto.crypto.provider.benchmarks;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+import com.amazon.corretto.crypto.provider.AmazonCorrettoCryptoProvider;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+public class KeyGenMLKEM {
+    @Param({"ML-KEM-512", "ML-KEM-768", "ML-KEM-1024"})
+    public String algorithm;
+    
+    @Param({AmazonCorrettoCryptoProvider.PROVIDER_NAME, "BC"})
+    public String provider;
+    
+    private KeyPairGenerator kpg;
+    
+    @Setup
+    public void setup() throws Exception {
+        BenchmarkUtils.setupProvider(provider);
+        kpg = KeyPairGenerator.getInstance(algorithm, provider);
+    }
+    
+    @Benchmark
+    public KeyPair generate() {
+        return kpg.generateKeyPair();
+    }
+
+}
