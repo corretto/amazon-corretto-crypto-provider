90945bcd24c1c8df0ccebde112e1a02a75c0b161
Will Childs-Klein <childw@amazon.com>
2025-10-08 15:40:19 -0400

    Register X25519 KeyAgreement, add tests

--- tst/com/amazon/corretto/crypto/provider/test/EvpKeyAgreementTest.java
+++ tst/com/amazon/corretto/crypto/provider/test/EvpKeyAgreementTest.java
@@ -68,6 +68,7 @@ public class EvpKeyAgreementTest {
     MASTER_PARAMS_LIST.add(buildEcdhParameters(new ECGenParameterSpec("NIST P-384"), "NIST P-384"));
     MASTER_PARAMS_LIST.add(buildEcdhParameters(new ECGenParameterSpec("NIST P-521"), "NIST P-521"));
     MASTER_PARAMS_LIST.add(buildEcdhParameters(EcGenTest.EXPLICIT_CURVE, "Explicit Curve"));
+    MASTER_PARAMS_LIST.add(buildX25519Parameters());
   }
 
   // No need to keep these in memory
@@ -158,6 +159,17 @@ public class EvpKeyAgreementTest {
         BC_PROV,
         Arrays.asList(
             buildKeyAtInfinity(pubKey), buildKeyOffCurve(pubKey), buildKeyOnWrongCurve(pubKey)));
+  }
+
+  private static TestParams buildX25519Parameters()
+      throws GeneralSecurityException, IOException {
+    return new TestParams(
+        "X25519",
+        "ECDH(X25519)",
+        KeyPairGenerator.getInstance("X25519", NATIVE_PROVIDER),
+        NATIVE_PROVIDER,
+        BC_PROV,
+        Arrays.asList());
   }
 
   static ECPublicKey buildKeyOffCurve(final ECPublicKey goodKey) throws GeneralSecurityException {
